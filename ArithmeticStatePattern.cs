9b7eca19b18317f494b4b9efcd858bd9c13041d9
GroupTheorist12 <55472994+GroupTheorist12@users.noreply.github.com>
2020-04-25 07:57:41 -0400

    new math

--- ArithmeticStatePattern.cs
+++ ArithmeticStatePattern.cs
@@ -30,7 +30,7 @@ namespace MatrixCalculus
     {
         private static Hashtable htTestFuncs = new Hashtable();
 
-        public static void CompileArithmetic()       
+        public static void CompileArithmetic()
         {
             System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
         }
@@ -47,9 +47,9 @@ namespace MatrixCalculus
             // write method names to hash
             foreach (MethodInfo methodInfo in methodInfos)
             {
-                List<string> ArithmeticOps = new List<string>() {"Add_", "Multiply_", "Subtract_", "Divide_"};
+                List<string> ArithmeticOps = new List<string>() { "Add_", "Multiply_", "Subtract_", "Divide_" };
 
-                int ind =  ArithmeticOps.FindIndex(a => methodInfo.Name.StartsWith(a));
+                int ind = ArithmeticOps.FindIndex(a => methodInfo.Name.StartsWith(a));
                 if (ind == -1)
                 {
                     continue;
@@ -78,11 +78,36 @@ namespace MatrixCalculus
             return sym.NakedTokenString;
         }
 
+        public static string AddVars(Symbol sym)
+        {
+            string ret = string.Empty;
+            string LeftL = string.Empty;
+            string RightL = string.Empty;
+            string LeftV = string.Empty;
+            string RightV = string.Empty;
+            List<string> LitBuf = new List<string>();
+            List<string> VarBuf = new List<string>();
+
+            for (int i = 0; i < sym.Tokens.Count; i++)
+            {
+                Token t = sym.Tokens[i];
+
+                if (t.Type == "Literal")
+                {
+                    LitBuf.Add(t.Value);
+                }
+                else if (t.Type == "Variable")
+                {
+                    VarBuf.Add(t.Value);
+                }
+            }
+            return ret;
+        }
         public static string Add_VariableOperator_Plus_Variable(Symbol sym)
         {
             string FunctionString = sym.NakedTokenString;
 
-            if(sym.Tokens[0].Value == sym.Tokens[2].Value)
+            if (sym.Tokens[0].Value == sym.Tokens[2].Value)
             {
                 FunctionString = string.Format("2{0}", sym.Tokens[0].Value);
             }
@@ -93,11 +118,62 @@ namespace MatrixCalculus
         {
             string FunctionString = sym.NakedTokenString;
 
-            if(sym.Tokens[0].Value == sym.Tokens[2].Value)
+            if (sym.Tokens[0].Value == sym.Tokens[2].Value)
             {
                 FunctionString = "0";
             }
             return FunctionString;
         }
+
+        public static string Add_LiteralOperatorMulVariableOperator_Plus_Variable(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+        public static string Add_VariableOperator_Plus_LiteralOperatorMulVariable(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+        public static string Add_LiteralOperatorMulVariableOperator_Plus_LiteralOperatorMulVariable(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+        public static string Add_VariableOperatorCaretLiteralOperator_Plus_VariableOperatorCaretLiteral(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+        public static string Add_LiteralOperatorMulVariableOperatorCaretLiteralOperator_Plus_VariableOperatorCaretLiteral(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+        public static string Add_VariableOperatorCaretLiteralOperator_Plus_LiteralOperatorMulVariableOperatorCaretLiteral(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+        public static string Add_LiteralOperatorMulVariableOperatorCaretLiteralOperator_Plus_LiteralOperatorMulVariableOperatorCaretLiteral(Symbol sym)
+        {
+            string AddString = sym.Expression;
+
+            return AddString;
+        }
+
+
     }
 }
